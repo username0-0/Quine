34b8e7c84064b869b58418f15b381596f5a7d18d
0010 <test_email>
2021-03-24 20:40:04 +0800

    check.sh

---
+++ check.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+./QuinePyHs_0.py > QuinePyHs_0.hs\
+&&\
+runhaskell QuinePyHs_0.hs > QuinePyHs_0.py
+
+#cat QuinePyHs_0.py
+#cat QuinePyHs_0.hs
+
+#git diff
+#git add QuinePyHs_0.hs
+#git add QuinePyHs_0.py
