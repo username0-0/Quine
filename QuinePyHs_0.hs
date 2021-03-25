94ef198e0014d9ed467b8e8fe68a9a57a7dedaa8
0010 <test_email>
2021-03-25 14:06:21 +0800

    +

--- QuinePyHs_0.hs
+++ QuinePyHs_0.hs
@@ -9,34 +9,37 @@ s=[
     "def f(x):",
     "    return q+x+q",
     "ss=[]",
-    "for x in [21,2]:",
+    "a, b, c = 0, len(s), 11",
+    "for x in [b-c,b-c+1]:",
     "    ss.append(s[x])",
-    "a,b=0,len(s)",
     "while a<b-1:",
     "    ss.append(s[3]+f(s[a])+chr(44))",
     "    a += 1",
     "ss.append(s[3]+f(s[a])+chr(93))",
-    "for x in [0,1,2,3,4,5,6,7,8]:",
-    "    ss.append(s[b-9+x])",
+    "a=0",
+    "while a<c-2:",
+    "    ss.append(s[b-c+2+a])",
+    "    a += 1",
     "#halt with <A_2B_2>",
     "for x in ss:",
     "    print(x)",
     "-- A_2 -> <C>",
+    "s=[",
     "-- B_2(<C>) -> <A_1B_1>",
     "g::String -> String",
-    "g x = (s!!3 ++) . flip (++) ([toEnum 44]) . show $ x",
+    "g x = (s!!3 ++) . flip (++) [','] . show $ x",
     "f::[String] -> [String]",
-    "f x = ((map g) $ init x) ++ [(flip (++) [toEnum 93] . (init.g) $ last x)]",
+    "f x = ((map g) $ init x) ++ [flip (++) [']'] . (init.g) $ last x]",
     "ss::[String]",
-    "ss=take 3 s ++ f s ++ (take 17 $ drop 4 s)",
+    "ss=take 3 s ++ f s ++ (take 19 $ drop 4 s)",
     "-- halt with <A_1B_1>",
     "main=putStrLn.unlines $ ss"]
 -- B_2(<C>) -> <A_1B_1>
 g::String -> String
-g x = (s!!3 ++) . flip (++) ([toEnum 44]) . show $ x
+g x = (s!!3 ++) . flip (++) [','] . show $ x
 f::[String] -> [String]
-f x = ((map g) $ init x) ++ [(flip (++) [toEnum 93] . (init.g) $ last x)]
+f x = ((map g) $ init x) ++ [flip (++) [']'] . (init.g) $ last x]
 ss::[String]
-ss=take 3 s ++ f s ++ (take 17 $ drop 4 s)
+ss=take 3 s ++ f s ++ (take 19 $ drop 4 s)
 -- halt with <A_1B_1>
 main=putStrLn.unlines $ ss
